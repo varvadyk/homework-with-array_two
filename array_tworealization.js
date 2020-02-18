e41052d2418754d9364d01a7f21c3e314af607fd
varvadyk <39148487+varvadyk@users.noreply.github.com>
2020-02-18 12:54:27 +0200

    change functions getMedian,filterEvenNumbers,countPositiveNumbers

--- array_tworealization.js
+++ array_tworealization.js
@@ -36,7 +36,7 @@ document.writeln(`<p> Function #3 - counts arithmetic mean  :${getAverage(6, 2, 
     //function 4
     function getMedian (...numbers) {
         let median = 0;
-        const sorted  = numbers.sort((a, b) => a -b);
+         numbers.sort((a, b) => a -b);
         let properIndex = 0;
         if (numbers.length % 2 !== 0) {
             properIndex = Math.floor(numbers.length / 2);
@@ -53,8 +53,8 @@ document.writeln(`<p> Function #3 - counts arithmetic mean  :${getAverage(6, 2, 
 
 //function 5
 function filterEvenNumbers(...numbers){
-const filter_result=numbers.filter(element => element % 2 !== 0);
-return filter_result;
+const filterResult=numbers.filter(element => element % 2 !== 0);
+return filterResult;
 }
 document.writeln(`<p> Function #5 - Filters even numbers:${filterEvenNumbers(1, 2, 3, 4, 5, 6)}</p>`);
 
@@ -62,12 +62,8 @@ document.writeln(`<p> Function #5 - Filters even numbers:${filterEvenNumbers(1, 
 //function 6
 function countPositiveNumbers  (...numbers) {
     const intNum = numbers.filter(num => Number.isInteger(num));
-    return intNum.reduce((count, num) => {
-        if (num > 0) {
-            count++;
-        }
-        return count;
-    }, 0)
+
+return intNum.filter(element => element >= 0).length;
 }
 document.writeln(`<p> Function #6 - Count the number of numbers greater than 0:${countPositiveNumbers(1, -2, 3, -4, -5, 6)}</p>`);
 
@@ -75,16 +71,14 @@ document.writeln(`<p> Function #6 - Count the number of numbers greater than 0:$
  function getDividedByFive(...numbers){
      return numbers.filter(element => element % 5 === 0)
  }
- document.writeln(`<p> Function #7 - filter  all the elements in the array and leave only completely divided by 5: ${countPositiveNumbers(1, -2, 3, -4, -5, 6)}</p>`);
-
- console.log(getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+ document.writeln(`<p> Function #7 - filter  all the elements in the array and leave only completely divided by 5: ${getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)}</p>`);
 
  //function 8
  function replaceBadWords(string){
     const arr = string.split(' ').map(word => {
-        const bad_words = /fuck|shit/i;
-        if (word.match(bad_words)) {
-            return word.replace(bad_words, '****');
+        const badWords = /fuck|shit/i;
+        if (word.match(badWords)) {
+            return word.replace(badWords, '****');
         } else {
             return word;
         }
@@ -93,8 +87,9 @@ document.writeln(`<p> Function #6 - Count the number of numbers greater than 0:$
 }
 document.writeln(`<p> Function #8 - Replace bad words with asterisks: ${replaceBadWords("Are you fucking kidding?")}</p>`);
 
-console.log(replaceBadWords("Are you fucking kidding?"));
- //function 9
+
+ 
+//function 9
  function divideByThree(word){
      const result =word.match(/.{1,3}/g);
      return result;
